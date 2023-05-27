3baecbb0dc155b3d10c51637325d08b98099d2f3
1amSumit <sj779619@gmail.com>
2023-05-27 14:32:53 +0530

    Fetched data

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -2,31 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-let events;
-
 const EventPage = () => {
   const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const res = await fetch("http://localhost:8080/events");
 
       if (!res.ok) {
-        throw new Error("Somethimg went wrong!");
+        setError("Something went wrong!");
+      } else {
+        const data = await res.json();
+        setEvents(data.events);
       }
-
-      const data = await res.json();
-      setEvents(data.events);
+      setIsLoading(false);
     };
-    try {
-      fetchData();
-    } catch (err) {
-      console.log(err.message);
-    }
+    fetchData();
   }, []);
 
   return (
     <>
       <h1>Events</h1>
+      {isLoading && (
+        <p style={{ textAlign: "center", fontSize: "32px" }}>
+          Loading Events...
+        </p>
+      )}
+      {error && <p>{error.message}</p>}
       <ul>
         {events.map((event) => (
           <li key={event.id}>
