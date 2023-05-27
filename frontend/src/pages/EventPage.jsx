b2a6b9b4d7dd1f4bbfa41d467fc4e6869bb26658
1amSumit <sj779619@gmail.com>
2023-05-27 14:20:24 +0530

    Another commit

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const events = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-  { id: "e4", title: "Event 4" },
-];
+let events;
 
 const EventPage = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8080/events");
+
+      if (!res.ok) {
+        throw new Error("Somethimg went wrong!");
+      }
+
+      const data = await res.json();
+      setEvents(data.events);
+    };
+    try {
+      fetchData();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, []);
+
   return (
     <>
       <h1>Events</h1>
       <ul>
         {events.map((event) => (
-          <li>
+          <li key={event.id}>
             <Link to={event.id}>{event.title}</Link>
           </li>
         ))}
